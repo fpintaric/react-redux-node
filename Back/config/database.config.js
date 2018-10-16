adeb95249cb8ec18f8f40e6fa44627e42d45185a
Franjo <franjo.pintaric.1909@gmail.com>
2018-10-16 18:52:41 +0200

    DB Setup
    Setup an mLab db and a connection to it

---
+++ Back/config/database.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  url: "mongodb://admin:admin1909@ds233763.mlab.com:33763/react-learning"
+};
--- Back/server.js
+++ Back/server.js
@@ -8,6 +8,26 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+const dbConfig = require("./config/database.config.js");
+const mongoose = require("mongoose");
+
+mongoose.Promise = global.Promise;
+
+mongoose
+  .connect(
+    dbConfig.url,
+    {
+      useNewUrlParser: true
+    }
+  )
+  .then(() => {
+    console.log("Successfully connected to the database");
+  })
+  .catch(err => {
+    console.log("Could not connect to the database. Exiting now...", err);
+    process.exit();
+  });
+
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
