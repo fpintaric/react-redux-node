567cef4cb772f221d7a5a460d81d256fe4126b65
Franjo <franjo.pintaric.1909@gmail.com>
2018-10-16 18:42:05 +0200

    Backend setup
    Seperated front / back, inited the backend express API

---
+++ Back/server.js
@@ -0,0 +1,26 @@
+const PORT = 8080;
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.json({ message: "You hit the root route!" });
+});
+
+app.listen(PORT, () => {
+  console.log("Server listening on port: ", PORT);
+});
